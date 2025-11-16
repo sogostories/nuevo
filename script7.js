be93968ba0575cc85209c3eb3b4c5dee189c0dd8
sogostories <storiesonthegosogo@gmail.com>
2025-11-16 17:55:37 +0100

    r

--- script7.js
+++ script7.js
@@ -26,7 +26,7 @@ const translations7 = [
 ];
 
 
-const startTimes7=[4,15,25,32,39,48,55,57,62,73,75,80,88,93,101,109,116,122,128,140,143,152,164,167,171,176];
+const startTimes7=[6.5,15,25,32,39,48,54.5,58,62,73,84,88,93,101,109,116,122,128,140,143,152,164,167,171,176];
 const vocabulary7 = [
   { word: 'Lina', translation: 'Lina' },
   { word: 'hadde', translation: 'had' },
